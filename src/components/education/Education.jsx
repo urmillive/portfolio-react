cda3a9f0c42ba708d9961f88f45964bb36fd5dc8
urmil404 <urmil8989@gmail.com>
2024-02-27 09:22:34 +0530

    updated the timeline for education

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -6,44 +6,47 @@ const Education = () => {
         <>
             <div className="timeline_wrapper">
                 <div className="timeline" id="timeline">
-                    <div className="container left">
+                    <div className="box left">
                         <div className="content">
-                            <h2>2016</h2>
-                            <p>I started coding when i was in 10<sup>th</sup> standard.</p>
+                            <h2>2015</h2>
+                            <p>Initiated my coding journey during 10<sup>th</sup> standard.</p>
                         </div>
                     </div>
-                    <div className="container right">
+                    <div className="box right">
                         <div className="content">
-                            <h2>2017</h2>
-                            <p>I completed C++ when during my 12<sup>th</sup> standard</p>
+                            <h2>2017 - Mastering C++ Prowess</h2>
+                            <p>Successfully completed C++ during 12<sup>th</sup> standard.</p>
                         </div>
                     </div>
-                    <div className="container left">
+                    <div className="box left">
                         <div className="content">
-                            <h2>2018</h2>
-                            <p>I started BCA and bought my 1<sup>st</sup> laptop.I stared coding in python and make projects on
-                                AI.</p>
+                            <h2>2018 - Coding Enthusiast Takes Flight</h2>
+                            <p>Embarked on BCA, acquired the 1st laptop, and delved into Python projects.</p>
                         </div>
                     </div>
-                    <div className="container right">
+                    <div className="box right">
                         <div className="content">
-                            <h2>2021</h2>
-                            <p>Participated in hackthon and local events for coding.Made 12+ project in various fields in IT
-                                industry.
-                                also Started MCA from Parul Universtiy.
+                            <h2>2021 - Coding Odyssey Begins</h2>
+                            <p>Active participation in hackathons, crafting 12+ projects across diverse IT domains. Commenced MCA at Parul University.
                             </p>
                         </div>
                     </div>
-                    <div className="container left">
+                    <div className="box left">
                         <div className="content">
-                            <h2>2022</h2>
-                            <p>Won my first Hackthon arranged by gateway group of companies.</p>
+                            <h2>2022 - Triumph at Gateway</h2>
+                            <p>Secured victory in the first Hackathon organized by Gateway Group of Companies..</p>
                         </div>
                     </div>
-                    <div className="container right">
+                    <div className="box right">
                         <div className="content">
-                            <h2>2023</h2>
-                            <p>Placed in Asite and working on new oportunities in AsiteSolutions..</p>
+                            <h2>2023 - Joining Asite Solutions</h2>
+                            <p>Initiated an enriching journey at Asite Solutions, exploring new opportunities and contributing to impactful projects.</p>
+                        </div>
+                    </div>
+                    <div className="box left">
+                        <div className="content">
+                            <h2>Present - 2024</h2>
+                            <p>Continuing the journey at Asite Solutions, contributing to innovative projects and solutions.</p>
                         </div>
                     </div>
                 </div>
