25dc95b6693c4d298888f6891c831eba352e1e0a
urmil404 <urmil8989@gmail.com>
2024-05-18 04:36:57 +0530

    Update bannerUrl in Snake Game project

--- src/projectsData.js
+++ src/projectsData.js
@@ -13,7 +13,7 @@ const projects = [
     {
         title: 'Snake Game',
         logoUrl: 'https://cdn2.iconfinder.com/data/icons/designer-skills/128/code-programming-javascript-software-develop-command-language-1024.png',
-        bannerUrl: 'https://urmillive.github.io/snakegame/snake.png',
+        bannerUrl: 'https://www.hdwallpapers.in/download/yellow_snake_in_black_background_hd_yellow-HD.jpg',
         description: 'Beginner Level Snake Game inspired from Nokia Old keypad Phones',
         githubLink: 'https://github.com/urmillive/snakegame',
         visitLink: 'https://urmillive.github.io/snakegame/'
