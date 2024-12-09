7d25e7cf90729cc93eec8c2e47576877411d6f4d
urmillive <urmillive@gmail.com>
2024-12-09 09:04:28 +0530

    Add E-learning project to projectsData with details and links

--- src/projectsData.js
+++ src/projectsData.js
@@ -3,6 +3,14 @@
 
 const projects = [
     {
+        title: 'E-learning',
+        logoUrl: 'https://elearning-imrx-hpkyj7ito-urmil404s-projects.vercel.app/images/logo-dark.png',
+        bannerUrl: 'https://images.unsplash.com/photo-1588912914074-b93851ff14b8?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        description: 'This eLearning platform, built with the MERN stack and Tailwind CSS, enables admins to manage users, content, and certification tests, while users can access blogs, run a code editor, and take paid certification tests.It offers a comprehensive solution for learning and certification management.',
+        githubLink: 'https://github.com/urmillive/Elearning',
+        visitLink: 'https://elearning-imrx-hpkyj7ito-urmil404s-projects.vercel.app/'
+    },
+    {
         title: 'TicTacToe Game',
         logoUrl: 'https://cdn0.iconfinder.com/data/icons/logos-brands-in-colors/128/react_color-1024.png',
         bannerUrl: 'https://images.pexels.com/photos/3400795/pexels-photo-3400795.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
