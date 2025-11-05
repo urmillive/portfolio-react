2537c85c024febf90dc85c53a627a8be3ed211ef
urmillive <urmillive@gmail.com>
2025-11-05 08:31:02 +0530

    fix: update E-learning project URLs to new deployment

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,32 +1,37 @@
-
 // projectsData.js
 
 const projects = [
-    {
-        title: 'E-learning',
-        logoUrl: 'https://elearning-imrx-hpkyj7ito-urmil404s-projects.vercel.app/images/logo-dark.png',
-        bannerUrl: 'https://images.unsplash.com/photo-1588912914074-b93851ff14b8?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        description: 'This eLearning platform, built with the MERN stack and Tailwind CSS, enables admins to manage users, content, and certification tests, while users can access blogs, run a code editor, and take paid certification tests.It offers a comprehensive solution for learning and certification management.',
-        githubLink: 'https://github.com/urmillive/Elearning',
-        visitLink: 'https://elearning-imrx-hpkyj7ito-urmil404s-projects.vercel.app/'
-    },
-    {
-        title: 'TicTacToe Game',
-        logoUrl: 'https://cdn0.iconfinder.com/data/icons/logos-brands-in-colors/128/react_color-1024.png',
-        bannerUrl: 'https://images.pexels.com/photos/3400795/pexels-photo-3400795.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        description: 'Beginner Level Tic Tac Toe game',
-        githubLink: 'https://github.com/urmillive/TictactoeGameReact',
-        visitLink: 'https://urmillive.github.io/TictactoeGameReact/'   
-    },
-    {
-        title: 'Snake Game',
-        logoUrl: 'https://cdn2.iconfinder.com/data/icons/designer-skills/128/code-programming-javascript-software-develop-command-language-1024.png',
-        bannerUrl: 'https://www.hdwallpapers.in/download/yellow_snake_in_black_background_hd_yellow-HD.jpg',
-        description: 'Beginner Level Snake Game inspired from Nokia Old keypad Phones',
-        githubLink: 'https://github.com/urmillive/snakegame',
-        visitLink: 'https://urmillive.github.io/snakegame/'
-    }
+  {
+    title: "E-learning",
+    logoUrl: "https://elearning-steel-three.vercel.app/images/logo-dark.png",
+    bannerUrl:
+      "https://images.unsplash.com/photo-1588912914074-b93851ff14b8?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description:
+      "This eLearning platform, built with the MERN stack and Tailwind CSS, enables admins to manage users, content, and certification tests, while users can access blogs, run a code editor, and take paid certification tests.It offers a comprehensive solution for learning and certification management.",
+    githubLink: "https://github.com/urmillive/Elearning",
+    visitLink: "https://elearning-steel-three.vercel.app/",
+  },
+  {
+    title: "TicTacToe Game",
+    logoUrl:
+      "https://cdn0.iconfinder.com/data/icons/logos-brands-in-colors/128/react_color-1024.png",
+    bannerUrl:
+      "https://images.pexels.com/photos/3400795/pexels-photo-3400795.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    description: "Beginner Level Tic Tac Toe game",
+    githubLink: "https://github.com/urmillive/TictactoeGameReact",
+    visitLink: "https://urmillive.github.io/TictactoeGameReact/",
+  },
+  {
+    title: "Snake Game",
+    logoUrl:
+      "https://cdn2.iconfinder.com/data/icons/designer-skills/128/code-programming-javascript-software-develop-command-language-1024.png",
+    bannerUrl:
+      "https://www.hdwallpapers.in/download/yellow_snake_in_black_background_hd_yellow-HD.jpg",
+    description:
+      "Beginner Level Snake Game inspired from Nokia Old keypad Phones",
+    githubLink: "https://github.com/urmillive/snakegame",
+    visitLink: "https://urmillive.github.io/snakegame/",
+  },
 ];
-
 
 export default projects;
